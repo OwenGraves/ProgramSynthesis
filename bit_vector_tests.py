1560edbde2acb4950455551ef293538709b712e3
OwenGraves <ograves.owen@gmail.com>
2021-01-24 11:51:06 -0600

    refactor and support two argument tests

--- bit_vector_tests.py
+++ bit_vector_tests.py
@@ -1,38 +1,57 @@
 from z3 import *
 
-class BVT:
-    @staticmethod
-    def P1(x): # Turn-off rightmost 1 bit.
-        o1 = x - 1
-        res = x & o1
-        return res
+def check_test(test, output, input, input2=None):
+    s = Solver()
+    x = BitVecVal(input, 32)
+    if input2 is not None:
+        y = BitVecVal(input2, 32)
+        s.add(test(x, y) == output)
+    else:
+        s.add(test(x) == output)
+    return s.check()
 
-    @staticmethod
-    def P2(x): # Test whether an unsigned integer is of the form 2^n - 1
-        o1 = x + 1
-        res = x & o1
-        return res
+def eval_test(test, input, input2=None):
+    s = Solver()
+    x = BitVecVal(input, 32)
+    output = BitVec('output', 32)
+    if input2 is not None:
+        y = BitVecVal(input2, 32)
+        s.add(test(x, y) == output)
+    else:
+        s.add(test(x) == output)
+    s.check()
+    return s.model()[output]
 
-    @staticmethod
-    def check_test(test, input, output, input2=None):
-        s = Solver()
-        x = BitVecVal(input, 32)
-        if input2 is not None:
-            y = BitVecVal(input2, 32)
-            s.add(test(x, y) == output)
-        else:
-            s.add(test(x) == output)
-        return s.check()
+def P1(x):
+    """Turn-off rightmost 1 bit."""
+    o1 = x - 1
+    res = x & o1
+    return res
 
-    @staticmethod
-    def eval_test(test, input, input2=None):
-        s = Solver()
-        x = BitVecVal(input, 32)
-        output = BitVec('output', 32)
-        if input2 is not None:
-            y = BitVecVal(input2, 32)
-            s.add(test(x, y) == output)
-        else:
-            s.add(test(x) == output)
-        s.check()
-        return s.model()[output]
+def P2(x):
+    """Test whether an unsigned integer is of the form 2^n - 1, returns 0 if yes."""
+    o1 = x + 1
+    res = x & o1
+    return res
+
+def P3(x):
+    """Isolate the rightmost 1-bit."""
+    o1 = -x
+    res = x & o1
+    return res
+
+# TODO figure out bitwise inequalities?
+def P10(x, y):
+    """Test if nlz(x) == nlz(y) where nlz is number of leading zeroes."""
+    o1 = x & y
+    o2 = x ^ y
+    res = o2 <= o1 # error
+    return res
+
+def P15(x, y):
+    """Floor of average of two integers without overflowing."""
+    o1 = x & y
+    o2 = x ^ y
+    o3 = o2 >> 1
+    res = o1 + o3
+    return res
--- program_synthesis.py
+++ program_synthesis.py
@@ -1,17 +1,18 @@
 from z3 import *
 from component import Component
-from bit_vector_tests import BVT
+import bit_vector_tests as BVT
 
-s = Solver()
+# print(BVT.check_test(BVT.P1, 0b1000, 0b1010))
+# print(BVT.eval_test(BVT.P1, 0b1010))
+# print(BVT.eval_test(BVT.P2, 0b11111))
+# print(BVT.check_test(BVT.P3, 0b100, 0b10100))
+# print(BVT.eval_test(BVT.P15, 1, 20))
 
-print(BVT.check_test(BVT.P1, 0b1010, 0b1000))
-print(BVT.eval_test(BVT.P1, 0b1010))
-print(BVT.eval_test(BVT.P2, 0b1010))
+# s = Solver()
 
-
-x, y = Ints('x y')
-c = Component.add([x,y], 5)
-s.add(c.constraint())
-print(s.check())
-print(s.model())
-print(s)
+# x, y = Ints('x y')
+# c = Component.add([x,y], 5)
+# s.add(c.constraint())
+# print(s.check())
+# print(s.model())
+# print(s)
