c9ec7345653325c1aec6777675e046e4086cb71d
OwenGraves <ograves.owen@gmail.com>
2021-02-01 18:32:59 -0600

    cleaned up generate constrained program

--- program.py
+++ program.py
@@ -10,7 +10,7 @@ class Program:
     def __init__(self, num_prog_inputs=1, components=[]):
         self.variable_numbers = Counter()
         self.prog_inputs = [self.fresh_I_variable() for _ in range(num_prog_inputs)]
-        self.prog_output = BitVec('R', BV_LENGTH)
+        self.prog_output = BitVec('result', BV_LENGTH)
         self.components = copy.deepcopy(components)
 
     def __str__(self):
@@ -28,13 +28,13 @@ class Program:
     def create_increment_component(self):
         return self.create_component(lambda x: x + 1, 1)
 
-    def generate_constrained_program(self):
+    def generate_constrained_program(self, inputs, output):
+        if not isinstance(inputs, list):
+            inputs = [inputs]
         P, R = [], []
-        O = []
         for c in self.components:
             P.extend(c.inputs)
             R.append(c.output)
-            O.append(self.fresh_O_variable())
         L = dict()
         for x in P + R:
             L[x] = self.fresh_l_variable()
@@ -45,18 +45,17 @@ class Program:
         for i in range(I_size):
             decode[i] = self.prog_inputs[i]
         for i in range(I_size, M):
-            decode[i] = O[i - I_size] 
+            decode[i] = R[i - I_size] 
         
+        # syntactic well-formedness constraints
         psi_cons = []
         for x, y in itertools.combinations(R, 2):
             psi_cons.append(L[x] != L[y])
-
         psi_acyc = []
         for c in self.components:
             for x in c.inputs:
                 psi_acyc.append(L[x] < L[c.output])
-
-        psi_wfp = [] # syntactic well-formedness constraints
+        psi_wfp = []
         for x in P:
             psi_wfp.append(0 <= L[x])
             psi_wfp.append(L[x] < M)
@@ -65,41 +64,27 @@ class Program:
             psi_wfp.append(L[x] < M)
         psi_wfp += psi_cons + psi_acyc
 
+        # semantic constraints
         psi_conn = []
         for i, input in enumerate(self.prog_inputs):
             L[input] = i
         L[self.prog_output] = M - 1
         for x, y in itertools.combinations(P + R + self.prog_inputs + [self.prog_output], 2):
             psi_conn.append(Implies(L[x] == L[y], x == y))
-
         phi_lib = []
         for c in self.components:
             phi_lib.append(c.constraint())
-
-        result = 10
-        output_constraint = result == self.prog_output
-        input1 = 4
-        input1_constraint = input1 == self.prog_inputs[0]
-        print(input1_constraint)
-        print(output_constraint)
 
         s = Solver()
         s.add(psi_wfp)
         s.add(psi_conn)
         s.add(phi_lib)
-        s.add(output_constraint)
-        s.add(input1_constraint)
-        # for testing
-        # s.add(BitVec('l4', BV_LENGTH) == 2)
-        # s.add(BitVec('l1', BV_LENGTH) != 0)
-        # print(psi_wfp)
-        # print(psi_conn)
-        # print(phi_lib)
-
-        s.check()
+        s.add(output == self.prog_output)
+        s.add([inputs[i] == self.prog_inputs[i] for i in range(min(len(inputs), len(self.prog_inputs)))])
+        check = s.check()
+        if check != sat:
+            return False
         l_values = s.model()
-        print(self)
-        print(l_values)
 
         # Lval2Prog
         p = Program(I_size, self.components)
